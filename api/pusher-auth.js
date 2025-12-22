1aafed952a288b78ec5497bae2916082f00b0635
Barzo <burakaliz2@gmail.com>
2025-12-22 13:33:38 +0300

    Add files via upload

--- api/pusher-auth.js
+++ api/pusher-auth.js
@@ -1,21 +1,22 @@
 const Pusher = require("pusher");
 
 const pusher = new Pusher({
-  appId: "2093942",
-  key: "7c829d72a0184ee33bb3",
-  secret: "56cb48f3ce69e64e61da",
-  cluster: "eu",
-  useTLS: true
+  appId: "2093942", key: "7c829d72a0184ee33bb3", secret: "56cb48f3ce69e64e61da", cluster: "eu", useTLS: true
 });
 
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const { socket_id, channel_name, username } = req.body;
+  
+  // Presence kanalı için kullanıcı bilgisi şarttır
+  const presenceData = {
+    user_id: username, 
+    user_info: { username: username } 
+  };
 
-  // Kullanıcı bilgilerini Pusher'a bildiriyoruz
-const presenceData = {
-    user_id: username, // benzersiz id
-    user_info: { username: username } // script.js'in okuduğu yer
-};
-const authResponse = pusher.authorizeChannel(socketId, channel, presenceData);
-  res.send(auth);
+  try {
+    const auth = pusher.authenticate(socket_id, channel_name, presenceData);
+    res.send(auth);
+  } catch (error) {
+    res.status(403).send("Auth hatası");
+  }
 }

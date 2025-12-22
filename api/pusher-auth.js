9ea975d37267fa1aa5fc0dee886ae7c0528ec08e
Barzo <burakaliz2@gmail.com>
2025-12-22 13:58:08 +0300

    Add files via upload

--- api/pusher-auth.js
+++ api/pusher-auth.js
@@ -6,14 +6,17 @@ const pusher = new Pusher({
 
 export default function handler(req, res) {
     const { socket_id, channel_name, username } = req.body;
+    
+    // Burası çok önemli: user_id olarak senin veritabanındaki ismini gönderiyoruz
     const presenceData = {
-        user_id: username,
-        user_info: { username: username }
+        user_id: username, 
+        user_info: { name: username }
     };
+
     try {
         const auth = pusher.authenticate(socket_id, channel_name, presenceData);
         res.send(auth);
     } catch (error) {
-        res.status(403).send(error.message);
+        res.status(403).send("Pusher Auth Hatası");
     }
 }
